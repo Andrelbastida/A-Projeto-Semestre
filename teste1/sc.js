5aa7814c353f8fb89de4a7d5b0c9383b33423aa5
André Bastida <andrelbastida@yahoo.com.br>
2023-06-08 17:28:01 -0300

    pagina ZOOM OK

---
+++ teste1/sc.js
@@ -0,0 +1,33 @@
+/* ZOOM Projeto Curriculo */
+var caixa_curriculo = document.getElementById("projeto_curriculo");
+var tela_curriculo = document.getElementById('zoom_projeto_Curriculo')
+
+caixa_curriculo.addEventListener('click', zoom )
+
+function zoom(){
+    zoom_projeto_Curriculo.style.display='block'
+}
+var btnfechar =document.getElementById('btn_fechar')
+btnfechar.addEventListener('click',fechaTela)
+
+function fechaTela(){
+    zoom_projeto_Curriculo.style.display='none'
+}
+/* _______________________________________________________ */
+
+
+/* ZOOM Projeto _ */
+
+/* _______________________________________________________ */
+/* ZOOM Projeto _ */
+
+/* _______________________________________________________ */
+/* ZOOM Projeto _ */
+
+/* _______________________________________________________ */
+/* ZOOM Projeto _ */
+
+/* _______________________________________________________ */
+/* ZOOM Projeto _ */
+
+/* _______________________________________________________ */
